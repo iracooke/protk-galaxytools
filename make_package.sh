3999529700a564c52c9ef05c4a291983bb364138
Ira Cooke <iracooke@gmail.com>
2013-01-07 12:27:28 +1100

    Improved excludes in package script

--- make_package.sh
+++ make_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -f package.tar.bz2
-tar --exclude=package.tar --exclude=.hgcheck --exclude=.hg --exclude=make_package.sh --exclude=README.md -cvf package.tar ./ 
+tar --exclude=.DS_Store --exclude=package.tar --exclude=.hgcheck --exclude=.hg --exclude=make_package.sh --exclude=README.md -cvf package.tar ./ 
 bzip2 package.tar
