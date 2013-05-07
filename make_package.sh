ab7f8b20943e8cbe5863256e893f0086127d7f90
Ira Cooke <iracooke@gmail.com>
2013-05-07 12:52:58 +1000

    Cleanup dependencies

--- make_package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f package.tar.bz2
-tar --exclude=.DS_Store --exclude=package.tar --exclude=.hgcheck --exclude=.hg --exclude=make_package.sh --exclude=README.md -cvf package.tar ./ 
-bzip2 package.tar
