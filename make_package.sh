81c2c597cfd55b2de99eb3a46e18738ef78110ed
Ira Cooke <iracooke@gmail.com>
2013-06-09 08:37:24 -0500

    Update to protk-1.2.2

--- make_package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f package.tar.bz2
-tar --exclude=package.tar --exclude=.hgcheck --exclude=.hg --exclude=make_package.sh --exclude=README.md -cvf package.tar ./ 
-bzip2 package.tar
