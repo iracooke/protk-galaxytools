9740f527fdba104fbc7a69b29fb884ea84593874
Ira Cooke <iracooke@gmail.com>
2015-03-27 08:11:01 +1100

    Remove old bundle.sh files

--- msgfplus/bundle.sh
+++
@@ -1,1 +0,0 @@
-tar --exclude=.hg --exclude=.DS_Store --exclude=.hgcheck --exclude=.git --exclude=*.tar --exclude=*.bz2 --exclude=bundle.sh -cvf package.tar ./;bzip2 -f package.tar
--- xtandem/bundle.sh
+++
@@ -1,2 +0,0 @@
-tar --exclude=.DS_Store --exclude=.hgcheck --exclude=.git --exclude=*.tar --exclude=*.bz2 \
---exclude=bundle.sh --exclude=.hg -cvf package.tar ./;bzip2 -f package.tar
